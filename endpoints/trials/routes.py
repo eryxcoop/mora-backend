98adba1c144b15a8c3dd0eeb9aef0b759b7b27b4
maxisuppes <msuppes@eryx.co>
2020-06-27 21:32:45 -0300

    Added endpoint to trials

--- endpoints/trials/routes.py
+++ endpoints/trials/routes.py
@@ -8,10 +8,19 @@ from . import trials
 @trials.route("/trials", methods=["POST"])
 def register_trial():
     try:
-        TrialsCollection().add(request.get_json())
-        response = {"success": True}
+        TrialsCollection().add_many(request.get_json())
+        response = {"object": {}, "errors": []}
         status_code = 201
     except Exception as error:
-        response = {"success": False, "error_message": error.message}
+        response = {"object": {}, "errors": [error.message]}
         status_code = 500
+    return dumps(response), status_code
+
+
+@trials.route('/trials', methods=['GET'])
+def all_trials():
+    trials = TrialsCollection().all()
+    response = {"object": trials, "errors": []}
+    status_code = 200
+
     return dumps(response), status_code
--- models/collections.py
+++ models/collections.py
@@ -42,9 +42,18 @@ class TrialsCollection(object):
         self._validate(json_data)
         self._trials.insert_one(json_data)
 
+    def add_many(self, trials):
+        for trial in trials:
+            self._validate(trial)
+
+        self._trials.insert_many(trials)
+
+    def all(self):
+        return self._trials.find()
+
     def _validate(self, json_data):
         validate(instance=json_data, schema=self._json_schema())
-        self._validate_profile_data(json_data["phone_id"], json_data["profile_name"])
+        self._validate_profile_data(json_data["phoneId"], json_data["profileName"])
 
     def _validate_profile_data(self, phone_id, profile_name):
         profiles = ProfilesCollection()
@@ -55,31 +64,23 @@ class TrialsCollection(object):
         schema = {
             "type": "object",
             "properties": {
-                "phone_id": {"type": "string"},
-                "profile_name": {"type": "string"},
-                "system_version": {"type": "string"},
-                "system_language": {"type": "string"},
-                "app_version": {"type": "string"},
-                "app_language": {"type": "string"},
-                "level": {"type": "string"},
-                "operation_type": {"type": "string"},
-                "operand_1": {"type": "number"},
-                "operator": {"type": "string"},
-                "operand_2": {"type": "number"},
-                "correct_result": {"type": "number"},
-                "entered_result": {"type": "number"},
-                "correct": {"type": "boolean"},
-                "total_time": {"type": "number"},
-                "start_date": {"type": "array"},
-                "end_date": {"type": "array"},
-                "hints_available": {"type": "boolean"},
-                "hints_shown": {"type": "boolean"},
+                "phoneId": {"type": "string"},
+                "profileName": {"type": "string"},
+                "systemLanguage": {"type": "string"},
+                "appVersion": {"type": "string"},
+                "appLanguage": {"type": "string"},
+                "planet": {"type": "number"},
+                "mission": {"type": "number"},
+                "episode": {"type": "number"},
+                "question": {"type": "string"},
+                "correctAnswer": {"type": "number"},
+                "options": {"type": "array"},
+                "userAnswer": {"type": "number"},
+                "startTime": {"type": "number"},
+                "endTime": {"type": "number"}
             },
-            "required": ["phone_id", "profile_name", "system_version", "system_language",
-                         "app_version", "app_language", "level", "operation_type",
-                         "operand_1", "operator", "operand_2", "correct_result", "entered_result",
-                         "correct", "total_time", "start_date", "end_date", "hints_available",
-                         "hints_shown"],
+            "required": ["phoneId", "profileName", "planet", "mission", "episode", "question", "correctAnswer",
+                         "options", "userAnswer", "startTime", "endTime"],
             "additionalProperties": False
         }
         return schema
